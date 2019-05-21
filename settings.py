7db5d1c4717e692429035de79feae349164d811a
Akaash Chikarmane <avchikar97@gmail.com>
2019-05-21 03:16:11 -0500

    initial commit

---
+++ settings.py
@@ -0,0 +1,14 @@
+import os
+
+# The prefix that will be used to parse commands.
+# It doesn't have to be a single character!
+COMMAND_PREFIX = "n!"
+
+# The bot token. Keep this secret!
+BOT_TOKEN = "NDgwNjE2OTE4ODczNjY5NjMy.Dlqlzw.D34_v3ze5C7q903uiwfURdJSwjM"
+
+# The now playing game. Set this to anything false-y ("", None) to disable it
+NOW_PLAYING = COMMAND_PREFIX + "commands"
+
+# Base directory. Feel free to use it if you want.
+BASE_DIR = os.path.dirname(os.path.realpath(__file__))
